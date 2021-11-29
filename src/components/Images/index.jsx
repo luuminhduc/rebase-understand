126e674d485dda3cc90e67b2696ed9e9c3c61a55
Duc Luu <luuminhduc00@gmail.com>
2021-11-29 23:33:49 +0700

    add more images

--- src/components/Images/index.jsx
+++ src/components/Images/index.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Images = () => {
-  return <h1>Images</h1>;
+  return (
+    <div>
+      <h1>Images</h1>
+      <h2>Images</h2>
+    </div>
+  );
 };
 
 export default Images;
