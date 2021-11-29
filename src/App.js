0dedbd7553859d94b0f1594bc79f3a6c235a3091
Duc Luu <luuminhduc00@gmail.com>
2021-11-29 22:14:49 +0700

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Posts from "./components/Header/Posts";
 
@@ -7,6 +8,7 @@ const App = () => {
     <div>
       <Header />
       <Posts />
+      <Footer />
     </div>
   );
 };
